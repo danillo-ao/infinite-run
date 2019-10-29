b219db64104ee6587dbd0b6f819c1f06622b3694
Danillo <danillo.alves.o@gmail.com>
2019-10-29 02:03:56 -0300

    remove test

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
 // Require dependencies files
 import GameController from "./game-controller";
 import CanvasController from "./canvas-controller";
-
-const Canvas = new CanvasController();
-Canvas.render();
